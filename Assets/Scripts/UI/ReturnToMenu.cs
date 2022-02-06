382622f15f3be869a59037287ee3582e5a8c5369
Rodrigo Caetani <rocaetani@gmail.com>
2022-02-06 04:42:06 -0300

    UI Components

--- Assets/Scripts/UI/ReturnToMenu.cs
+++ Assets/Scripts/UI/ReturnToMenu.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StartButton : MonoBehaviour
+public class ReturnToMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void OnClick()
     {
-        
+        SceneController.Instance.LoadMenu();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
