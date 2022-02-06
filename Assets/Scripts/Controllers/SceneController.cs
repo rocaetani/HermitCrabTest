fa519e57fc5e2f114eabf7e8efb72d370ad5e44f
Rodrigo Caetani <rocaetani@gmail.com>
2022-02-06 04:40:13 -0300

    SceneController Async Load

--- Assets/Scripts/Controllers/SceneController.cs
+++ Assets/Scripts/Controllers/SceneController.cs
@@ -7,9 +7,48 @@ using UnityEngine.SceneManagement;
 public class SceneController : MonoBehaviour
 {
 
-    public void StartScene(String sceneName)
+    public static SceneController Instance;
+    
+    public delegate void OnPhaseLoadedDelegate(); 
+    public event OnPhaseLoadedDelegate OnPhaseLoaded;
+
+    //public static SceneController Instance;
+    private void Awake()
     {
-        SceneManager.LoadScene(sceneName);
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+
+    public void LoadPhase(String phaseName)
+    {
+        StartCoroutine(nameof(LoadPhaseAsync), phaseName);
+    }
+
+    public void LoadEndPhase()
+    {
+        SceneManager.LoadScene(TagManager.END_PHASE_SCENE_TAG);
+    }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(TagManager.MENU_SCENE_TAG);
+    }
+    
+    IEnumerator LoadPhaseAsync(String phaseName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(phaseName);
+        
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+        OnPhaseLoaded?.Invoke();
     }
 
 }
